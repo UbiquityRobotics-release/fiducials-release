b0ee4226e9f6faae18b05266aa83ac51b78caf36
Rohan Agrawal <send2arohan@gmail.com>
2019-05-09 13:19:08 -0700

    Rebase from 'upstream'

--- aruco_detect/scripts/create_markers.py
+++ aruco_detect/scripts/create_markers.py
@@ -1,11 +1,25 @@
 #!/usr/bin/python
 
-import os, sys
-import em
+import os, sys, argparse
+import subprocess
+import imp, importlib
+
+# Hack if a user has 'em' instead of 'empy' installed with pip
+# If the module em doesn't have the expand function use the full path
+em = None
+for path in sys.path:
+    filename = os.path.join(path, 'em.py')
+    if os.path.exists(filename):
+         em = imp.load_source('em', filename)
+         if "expand" in dir(em):
+             break
+# For-else: else is called if loop doesn't break 
+else:
+    print "ERROR: could not find module em, please sudo apt install python-empy"
+    exit(2)
 
 import cv2
 import cv2.aruco as aruco
-import cairosvg
 
 """
 Generate a PDF file containaing one or more fiducial marker for printing
@@ -18,7 +32,7 @@ def checkCmd(cmd, package):
     sudo apt install %s""" % (cmd, package)
         sys.exit(1)
      
-def genSvg(id, dicno):
+def genSvg(id, dicno, paper_size):
     return em.expand("""<svg width="@(paper_width)mm" height="@(paper_height)mm"
  version="1.1"
  xmlns:xlink="http://www.w3.org/1999/xlink"
@@ -66,41 +80,60 @@ def genSvg(id, dicno):
   <text x="@(paper_width/2)mm" y="@((paper_height + fid_len)/2 + 30)mm" text-anchor="middle" style="font-family:ariel; font-size:24;">@(id) D@(dicno)</text>
 
 </svg>
-""", {"id": id, "dicno": dicno, "paper_width": 215.9, "paper_height": 279.4, "fid_len": 140.0})
+""", {"id": id, "dicno": dicno, "paper_width": paper_size[0], "paper_height": paper_size[1], "fid_len": 140.0})
 
-def genMarker(i, dicno):
+def genMarker(i, dicno, paper_size):
     print " Marker %d\r" % i,
     sys.stdout.flush()
     aruco_dict = aruco.Dictionary_get(dicno)
     img = aruco.drawMarker(aruco_dict, i, 2000)
     cv2.imwrite("/tmp/marker%d.png" % i, img)
-    svg = genSvg(i, dicno)
-    cairosvg.svg2pdf(bytestring=svg, write_to='/tmp/marker%d.pdf' % i)
+    svg = genSvg(i, dicno, paper_size)
+    cairo = subprocess.Popen(('cairosvg', '-f', 'pdf', '-o', '/tmp/marker%d.pdf' % i, '/dev/stdin'), stdin=subprocess.PIPE)
+    cairo.communicate(input=svg)
+    # This way is faster than subprocess, but causes problems when cairosvg is installed from pip
+    # because newer versions only support python3, and opencv3 from ros does not
+    # cairosvg.svg2pdf(bytestring=svg, write_to='/tmp/marker%d.pdf' % i)
     os.remove("/tmp/marker%d.png" % i)
 
 if __name__ == "__main__":
     checkCmd("pdfunite", "poppler-utils")
-    checkCmd("cairosvg-py2", "python-cairosvg")
+    checkCmd("cairosvg", "python-cairosvg")
 
-    dicno = 7
-    argc = len(sys.argv)
-    if argc != 4 and argc != 5:
-        print "Usage: %s startId endId pdfFile [dictionary]" % sys.argv[0]
-        sys.exit(1)
-    outfile = sys.argv[3]
-    if argc == 5:
-        dicno = int(sys.argv[4])
-    markers = range(int(sys.argv[1]), int(sys.argv[2])+1)
+
+    parser = argparse.ArgumentParser(description='Generate Aruco Markers.')
+    parser.add_argument('startId', type=int,
+                        help='start of marker range to generate')
+    parser.add_argument('endId', type=int,
+                        help='end of marker range to generate')
+    parser.add_argument('pdfFile', type=str,
+                        help='file to store markers in')
+    parser.add_argument('dictionary', type=int, default='7', nargs='?',
+                        help='dictionary to generate from')
+    parser.add_argument('--paper-size', dest='paper_size', action='store',
+                        default='letter', help='paper size to use (letter or a4)')
+
+    args = parser.parse_args()
+
+    outfile = args.pdfFile
+    dicno = args.dictionary
+
+    markers = range(args.startId, args.endId + 1)
     pdfs = map(lambda i: "/tmp/marker%d.pdf" % i, markers)
+
+    if args.paper_size == 'letter':
+        paper_size  = (215.9, 279.4)
+    elif args.paper_size == 'a4':
+        paper_size  = (210, 297)
 
     try:
         # For a parallel version
         from joblib import Parallel, delayed
-        Parallel(n_jobs=-1)(delayed(genMarker)(i, dicno) for i in markers)
+        Parallel(n_jobs=-1)(delayed(genMarker)(i, dicno, paper_size) for i in markers)
     except ImportError:
         # Fallback to serial version
         for i in markers:
-            genMarker(i, dicno)
+            genMarker(i, dicno, paper_size)
 
     print "Combining into %s" % outfile
     os.system("pdfunite %s %s" % (" ".join(pdfs), outfile))
